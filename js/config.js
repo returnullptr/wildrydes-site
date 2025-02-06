dd6cd848c2c9bddc2449738c03fb6e4ec59b51f9
returnullptr <116039823+returnullptr@users.noreply.github.com>
2025-02-05 17:28:30 -0800

    Update config.js
    Updated App Client

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_po0RdWJhk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5s1nvk09ahvc16qdfqe7rsq73j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '481b2hhblbnevs1cuh5g0b3oja', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
